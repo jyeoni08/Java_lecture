6637c4bbff08cfced298a8a1317d6162c38fe638
ssjes <ssjes@DESKTOP-DQ8I40O>
2021-01-11 16:23:37 +0900

    first

---
+++ Java_lecture/src/com/urdomain/ch02/Hello.java
@@ -0,0 +1,9 @@
+package com.urdomain.ch02;
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Hello, welcome to the java world");
+	System.out.println("hello");
+}
+
+}
